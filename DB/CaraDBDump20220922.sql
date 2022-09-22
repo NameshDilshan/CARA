19e4247a97dc3c0d23181fc146f7514074930bf5
NameshDilshan <nameshdilshan@gmail.com>
2022-09-22 11:24:11 +0530

    Create CaraDBDump20220922.sql

---
+++ DB/CaraDBDump20220922.sql
@@ -0,0 +1,89 @@
+CREATE DATABASE  IF NOT EXISTS `caradb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `caradb`;
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: localhost    Database: caradb
+-- ------------------------------------------------------
+-- Server version	5.6.26-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'namesh','nameshdilshan@gmail.com','123');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `rating` varchar(45) DEFAULT NULL,
+  `price` varchar(45) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `isfeatured` varchar(45) DEFAULT NULL,
+  `islatest` varchar(45) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `category` varchar(45) DEFAULT NULL,
+  `image_two` varchar(255) DEFAULT NULL,
+  `image_three` varchar(255) DEFAULT NULL,
+  `image_one` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Gold Geometric Earrings','5','Rs590.00','images/product-1.jpg','1','0','New Simple Vintage Designed Metal Gold Hoop Geometric Earrings for Casual/Office Wearing.','Earrings','images/gallery-1.jpg','images/gallery-1.jpg','images/gallery-2.webp'),(2,'Gold 5pcs Ring set','3','Rs890.00','images/Product-2.jpg','1','0',NULL,NULL,NULL,NULL,NULL),(3,'Gold bow dangle Earrings','4','Rs530.00','images/Product-3.jpeg','1','0',NULL,NULL,NULL,NULL,NULL),(4,'2Layered Gold Necklace','4','Rs760.00','images/Product-4.jpeg','1','0',NULL,NULL,NULL,NULL,NULL),(5,'Gold Drop Earrings','3','Rs690.00','images/product-5.webp',NULL,'1',NULL,NULL,NULL,NULL,NULL),(6,'7 Pearl Hoop Earrings Set','4','Rs2300.00','images/product-6.webp',NULL,'1',NULL,NULL,NULL,NULL,NULL),(7,'Pearl Chocker Necklace','2','Rs950.00','images/product-7.jpg',NULL,'1',NULL,NULL,NULL,NULL,NULL),(8,'Gold simple Pearl Chocker','3','Rs490.00','images/product-8.webp',NULL,'1',NULL,NULL,NULL,NULL,NULL),(9,'Asymetric Daisy Earrings','4','Rs750.00','images/product-9.jpg',NULL,'1',NULL,NULL,NULL,NULL,NULL),(10,'Star Shell Drop Earrings','2','Rs520.00','images/product-10.webp',NULL,'1',NULL,NULL,NULL,NULL,NULL),(11,'Butterfly Stud Earrings','1','Rs470.00','images/product-11.webp',NULL,'1',NULL,NULL,NULL,NULL,NULL),(12,'Gold Pearl chain Ring','5','Rs650.00','images/product-12.jpg',NULL,'1',NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-22 11:23:03
